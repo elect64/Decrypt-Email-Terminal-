8d1fb213ef26b0a32c01ddab6ca050785456e627
elect <okezieelect4@gmail.com>
2026-09-13 13:19:17 +0100

    changed appscript link 0913 v3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Your deployed Google Apps Script Web App URL
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyHKasH5PO7eFU2lscek_mkew5BoU7FhkDxDLZTuzMGcvnKFBNTSVrxwZrZs9vU8J-z3Q/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyewL7-WGXdC1WtK8ycSAiLWhaaeaSUU2UafuRkNbGFIbzy7a3P1jpN9PhsgFxqfyR2LA/exec";
 
 /* -----------------------------------------------------------
    Theme
