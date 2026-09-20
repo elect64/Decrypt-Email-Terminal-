f35932371fac619cd1663a2f25e784bec3648ef9
elect <okezieelect4@gmail.com>
2026-09-20 10:15:51 +0100

    cdebugged success

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Your deployed Google Apps Script Web App URL
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9necQBsuDmL2QvCql_osp4A1kCcA0vjVp8RhMi-AMHLN5KMGEeFzr4Yu_d4Zu5IZ3Cg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwy6FTlIEmCRI3ioxKGhB9lbAuCt4d1FqzEqvT92rDG9Qk2sVOzhx2pr3vIZG4rR6PEzw/exec";
 
 /* -----------------------------------------------------------
    Theme
