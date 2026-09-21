50a00c11115c0dd6654c79f3806b1a5a0c31891c
elect <okezieelect4@gmail.com>
2026-09-22 00:18:51 +0100

    updated app look and app download log

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Your deployed Google Apps Script Web App URL
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwy6FTlIEmCRI3ioxKGhB9lbAuCt4d1FqzEqvT92rDG9Qk2sVOzhx2pr3vIZG4rR6PEzw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyHx0H6SbRvcvu7QWt5QJiLBVwl3kt2QSO0zxrBQJ0pPlbPdUNhiBRavtUylF0TiEI3aA/exec";
 
 /* -----------------------------------------------------------
    Theme
